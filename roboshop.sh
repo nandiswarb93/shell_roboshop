a044bba4808028623499ab995c8a055cdf615724
Busapalli Nandiswar <nandiswarb93@gmail.com>
2025-11-04 23:13:12 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -6,9 +6,9 @@ do
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids sg-07bc36bfdc3d14144 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     if [ $instance != "frontend" ]; then
-        IP=$(aws ec2 describe-instances --image-id $INSTANCE_ID --instance-type t3.micro --security-group-ids sg-07bc36bfdc3d14144 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].PrivateIpAddress' --output text)
+        IP=$(aws ec2 describe-instances --image-id $INSTANCE_ID --instance-type t3.micro --security-group-ids sg-07bc36bfdc3d14144 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
     else
-        IP=$(aws ec2 describe-instances --image-id $INSTANCE_ID --instance-type t3.micro --security-group-ids sg-07bc36bfdc3d14144 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].PublicIpAddress' --output text)
+        IP=$(aws ec2 describe-instances --image-id $INSTANCE_ID --instance-type t3.micro --security-group-ids sg-07bc36bfdc3d14144 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
     fi
 
 
